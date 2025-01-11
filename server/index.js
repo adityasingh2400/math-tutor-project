673067ca42c2206d0c1023873e6f0dfea1584703
Aditya Singh <adisin650@gmail.com>
2025-01-11 09:58:48 -0800

    4th commit

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
@@ -12,21 +12,27 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch((err) => console.error(err));
-
+/*mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error(err));
+*/
 // Basic Route
 app.get('/', (req, res) => {
   res.send('Hello from the Math Tutor API!');
 });
+
+// Import Routes
+const problemRoutes = require('./routes/problems');
+
+// Use Routes
+app.use('/api/problems', problemRoutes);
 
 // Start Server
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
