49401110d3696123540fcfb802911177f58eb912
Rudraksh Gupta <rudraksh.gupta200@gmail.com>
2025-01-11 11:46:05 -0800

    some ui updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,23 @@
 // src/App.jsx
 
 import React from 'react';
-<<<<<<< HEAD
 import SubmitProblem from './components/SubmitProblem.jsx'; // Ensure you've imported the new component
-=======
-import SubmitProblem from "./components/SubmitProblem.jsx"; // Ensure you've imported the new component
->>>>>>> b604d46c60ee7e92912d8687941654628da8b09e
+import Upload from './components/Upload';  // Import the Upload component
+
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to the Math Problem Finder</h1>
       <SubmitProblem />
+      <Upload />  {/* Include the Upload component */}
+
     </div>
   );
 }
 
+
+
 export default App;
+
+
